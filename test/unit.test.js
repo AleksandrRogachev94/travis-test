4d0232632c15b390778f89f4372d69cd5cecd6dc
Aleksandr <aleksandr.rogachev1994@gmail.com>
2017-09-27 21:47:41 -0400

    ggg

---
+++ test/unit.test.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe("test test test", function() {
+  it('handles all stuff', function(done) {
+    expect(true).to.be.true
+    done()
+  })
+})
