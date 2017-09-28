c59458c0fdd0d08aeefbb5120dd0304422612e9a
Aleksandr <aleksandr.rogachev1994@gmail.com>
2017-09-28 10:52:49 -0400

    lol2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(logger('dev'))
 app.get('/', function(req, res, next) {
   console.log("RIGHT HERE")
   res.status(200).json({
-    message: "Hello World10"
+    message: "Hello World11"
   })
 })
 
