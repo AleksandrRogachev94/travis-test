818736b3d24b279257709d18e3e8417df61d3e47
Aleksandr <aleksandr.rogachev1994@gmail.com>
2017-09-27 21:24:15 -0400

    1 change

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(logger('dev'))
 app.get('/', function(req, res, next) {
   console.log("RIGHT HERE")
   res.status(200).json({
-    message: "Hello World"
+    message: "Hello World2"
   })
 })
 
